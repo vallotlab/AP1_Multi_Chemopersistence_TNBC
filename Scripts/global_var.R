4631cd287c15e4ca79fae424a9f3710e96c25fa4
Gregoire Jouault <gjouault@Gregoires-MacBook-Pro.local>
2024-08-09 08:31:25 +0200

    First commit

--- Scripts/global_var.R
+++ Scripts/global_var.R
@@ -49,13 +49,15 @@ library(ggdendro)
 library(msigdbr)
 library(fgsea)
 library(Nebulosa)
+library(memes)
+library(universalmotif)
 
 
 ###### VARIABLES ######
 
 set.seed(1)
-PDX_scRNA_samples = c("HBCx172_Capecitabin", "HBCx172_chemonaive", "HBCx218_AC","HBCx218_chemonaive", "HBCx221_Capecitabin",
-                      "HBCx221_chemonaive","HBCx39_Capecitabin","HBCx39_chemonaive","HBCx95_Capecitabin","HBCx95_chemonaive")
+PDX_scRNA_samples = c("HBCx172_Capecitabine", "HBCx172_chemonaive", "HBCx218_AC","HBCx218_chemonaive", "HBCx221_Capecitabine",
+                      "HBCx221_chemonaive","HBCx39_Capecitabine","HBCx39_chemonaive","HBCx95_Capecitabine","HBCx95_chemonaive")
 
 s.genes <- cc.genes$s.genes
 g2m.genes <- cc.genes$g2m.genes
@@ -74,7 +76,7 @@ AnnColor_State = c("chemonaive" = "grey10",
             "Relapse" = "grey40")
 
 AnnShape_treatment = c("AC" = 21,
-                   "Capecitabin" = 22, 
+                   "Capecitabine" = 22, 
                    "Carboplatin" = 24, 
                    "chemonaive" = 3, 
                    "Cisplatin" = 23)
@@ -87,3 +89,43 @@ TFs_top10_Allsamples = c("IRF1", "STAT1", "AP1", "JUN", "NFKB1", "NFKB","RELA","
 List_genes_boxplot = c("KRT14", "CDH2", "JUNB", "E2F7", "ABCA1", "IDI1", "LDLR", "SQLE","IRF1", "STAT1", 
                        "JUN","FOS","NFKB2","RELA","JUND","FOSL1","FOSL2","FOSB","JUNB","NFKB1","REL",
                        "RELB","IRF3","IRF5","IRF7","IRF9","STAT2","STAT3","STAT5A","STAT5B")
+
+Cell_lines_samples = c("MM468_chemonaive", "MM468_5FU1_day33","MM468_5FU2_day67","MM468_5FU3_day50","MM468_5FU3_day77","BT20_chemonaive", "BT20_persister","HCC38_chemonaive", "HCC38_persister")
+
+List_MM468_persister = c("MM468_5FU1_day33","MM468_5FU2_day67","MM468_5FU3_day50","MM468_5FU3_day77")
+
+
+###### FUNCTIONS ######
+
+Associate_Annot = function(masterPeak, Annotations){
+  Peak$peak_loca = paste0(Peak@seqnames, "_",Peak@ranges)
+  genes.coords.all = read.table(file.path(Annotations, "EnsDB.All.bed.df"), header = T)
+  genes.coords.all$seqnames = paste0("chr", genes.coords.all$seqnames)
+  genes.coords.all = plyranges::as_granges(genes.coords.all)
+  
+  Peak = plyranges::join_overlap_inner(Peak, genes.coords.all)
+  Peak = as.data.frame(Peak)
+  Order_loca = c("Promoter_1K","Promoter_2K","Promoter_3K","Promoter_5K","TSS_1K","TSS_2K","TSS_5K","TSS_10K","Enhancers","Genes","Intergenic")
+  Peak$seq_coord_system = factor(Peak$seq_coord_system, levels = Order_loca)
+  Peak = Peak %>% dplyr::group_by(peak_loca) %>% dplyr::arrange(seq_coord_system, .by_group = T) %>% dplyr::slice_head(n = 1)
+  Peak = plyranges::as_granges(Peak)
+  
+  genes.coords.all_onlygenes = genes.coords.all %>% dplyr::filter(seq_coord_system == "Genes")
+  Intergenic_matdiff = Peak %>% dplyr::filter(seq_coord_system == "Intergenic") %>% plyranges::as_granges()
+  Intergenic_matdiff$Nearest_gene = genes.coords.all_onlygenes$gene_name[nearest(Intergenic_matdiff, genes.coords.all_onlygenes)]
+  Intergenic_matdiff = as.data.frame(Intergenic_matdiff)
+  gene_MatDiff_fullannot = Peak %>% dplyr::filter(seq_coord_system != "Intergenic")
+  gene_MatDiff_fullannot$Nearest_gene = gene_MatDiff_fullannot$gene_name
+  gene_MatDiff_fullannot = as.data.frame(gene_MatDiff_fullannot)
+  gene_MatDiff_fullannot = rbind(gene_MatDiff_fullannot, Intergenic_matdiff)
+  gene_MatDiff_fullannot = plyranges::as_granges(gene_MatDiff_fullannot)
+  return(gene_MatDiff_fullannot)
+}
+
+get_density <- function(x, y, ...) {
+  dens <- MASS::kde2d(x, y, ...)
+  ix <- findInterval(x, dens$x)
+  iy <- findInterval(y, dens$y)
+  ii <- cbind(ix, iy)
+  return(dens$z[ii])
+}
